9f4926520d4e4e83abb5a141cc8f64b578dfd85b
RAMKUMARAN T <ramkumaran.t@gmail.com>
2021-09-20 21:27:38 +0530

    README.md for certs-installer.jar added.

--- certs-installer/certs-installer.bat
+++
@@ -1,5 +0,0 @@
-cls 
-java -jar certs-installer.jar repo.maven.apache.org:443 changeit
-java -jar certs-installer.jar repo1.maven.org:443 changeit
-java -jar certs-installer.jar oss.sonatype.org:443 changeit
-java -jar certs-installer.jar repository.mulesoft.org:443 changeit
